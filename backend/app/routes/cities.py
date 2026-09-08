d312f7d7c4235aa2cab6ac99068b442d6988cc59
david <sarigoo421@gmail.com.il>
2026-09-08 15:48:02 +0300

    added pydantic

--- backend/app/routes/cities.py
+++ backend/app/routes/cities.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from app.services.open_meteo import search_cities
+from app.schemas.city import City
 
 
 router = APIRouter(prefix= "/cities")
 
-@router.get("/search")
+@router.get("/search", response_model=list[City])
 def search(name:str):
     
     data = search_cities(name)
--- backend/app/services/open_meteo.py
+++ backend/app/services/open_meteo.py
@@ -1,6 +1,23 @@
 import requests
+from app.schemas.city import City
 
 
 def search_cities(name):
-    city = requests.get("https://geocoding-api.open-meteo.com/v1/search", params={"name": name, "count":5 ,"language": "en", "format": "json"})
-    return city.json()
+    response = requests.get("https://geocoding-api.open-meteo.com/v1/search", params={ "name": name, "count": 5, "language": "en", "format": "json"})
+
+    data = response.json()
+    results = data.get("results", [])
+
+    cities = []
+
+    for c in results:
+        city = City(
+            name=c["name"],
+            latitude=c["latitude"],
+            longitude=c["longitude"],
+            country=c["country"]
+        )
+
+        cities.append(city)
+
+    return cities
