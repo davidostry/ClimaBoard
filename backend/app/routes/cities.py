885a4dc6a95d354241a85a907b45b5885d3afe58
david <sarigoo421@gmail.com.il>
2026-09-08 16:03:29 +0300

    added query

--- backend/app/routes/cities.py
+++ backend/app/routes/cities.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from app.services.open_meteo import search_cities
 from app.schemas.city import City
 
@@ -6,7 +6,7 @@ from app.schemas.city import City
 router = APIRouter(prefix= "/cities")
 
 @router.get("/search", response_model=list[City])
-def search(name:str):
+def search(name: str = Query(..., min_length=2, max_length=50)):
     
     data = search_cities(name)
     return data
