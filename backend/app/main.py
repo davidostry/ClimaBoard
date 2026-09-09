a731b035c26dc0532f1837eb844d801c8b056506
david <sarigoo421@gmail.com.il>
2026-09-09 17:26:31 +0300

    added corse

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
-from app.routes import cities, favorites, weather, atbash
+from fastapi.middleware.cors import CORSMiddleware
 
+from app.routes import cities
+from app.routes import favorites
+from app.routes import weather
+from app.routes import atbash
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(cities.router)
 app.include_router(favorites.router)
 app.include_router(weather.router)
 app.include_router(atbash.router)
+
 
 @app.get("/health")
 def health():
