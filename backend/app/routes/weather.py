490eb3b4153d1afc624de340601dbca69f9beb6e
david <sarigoo421@gmail.com.il>
2026-09-09 15:44:48 +0300

    added compare

--- backend/app/routes/weather.py
+++ backend/app/routes/weather.py
@@ -13,3 +13,9 @@ def search(latitude: float = Query(..., ge=-90, le=90), longitude: float= Query(
 def forecast(latitude: float = Query(..., ge=-90, le=90), longitude: float= Query(..., ge=-180, le=180), days: int = Query(..., ge=1, le=16)):
     data = get_weather(latitude, longitude, days)
     return data
+
+@router.get("/compare")
+def compare(latitude1: float = Query(..., ge=-90, le=90), longitude1: float= Query(..., ge=-180, le=180),latitude2: float = Query(..., ge=-90, le=90), longitude2: float= Query(..., ge=-180, le=180)):
+    city1 = get_weather(latitude1, longitude1)
+    city2 = get_weather(latitude2, longitude2)
+    return {"city1":city1, "city2":city2}
